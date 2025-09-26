86ef70bdeff5b38d81f005d962fabc2925ac7faf
julian.reis <jreis25@uw.edu>
2025-09-26 19:27:34 +0000

    added plotting

---
+++ Post-Processing/4_frame_tracking/particle_statistics.py
@@ -0,0 +1,47 @@
+# %%
+from python_4be_eti.load_tracks import load_tracks
+from mcflow_plotting.turbulence.pdf import plot_pdf, plot_pdf_log
+from mcflow_plotting.turbulence.velocity import plot_rms_velocity
+from mcflow_plotting.turbulence.velocity import plot_mean_vel_time
+import numpy as np
+
+
+filename = '/workspaces/4d-ptv-mcflow/data/julian/PTV_center/TTI_no_gravity/Run2/tracks.h5'
+output_path = '/workspaces/4d-ptv-mcflow/data/julian/PTV_center/TTI_no_gravity'
+
+samples = load_tracks(filename, 1e-3, 10)
+
+# %%
+track_lengths = [track.track_length for track in samples]
+mean_track_length = np.mean(track_lengths)
+print(f"Mean track length in samples: {mean_track_length}")
+print(f"Total number of tracks is {len(track_lengths)}")
+# %%
+time = [track.time for track in samples]
+velocity_magnitudes = [track.vmag for track in samples]
+plot_pdf(velocity_magnitudes, scale=1000,
+         variable='||V||2', output=output_path)
+plot_mean_vel_time(velocity_magnitudes, time, scale=1000,
+                   variable='||V||^2', output=output_path)
+# %% VELOCITY X
+velocity_x = [np.mean(track.vx) for track in samples]
+plot_pdf(velocity_x, scale=1000, variable='V_x', output=output_path)
+plot_mean_vel_time(velocity_x, time, scale=1000,
+                   variable='V_x', output=output_path)
+# %% VELOCITY Y
+velocity_y = [np.mean(track.vy) for track in samples]
+plot_pdf(velocity_y, scale=1000, variable='V_y', output=output_path)
+plot_mean_vel_time(velocity_y, time, scale=1000,
+                   variable='V_y', output=output_path)
+
+# %% VELOCITY Z
+velocity_z = [np.mean(track.vz) for track in samples]
+plot_pdf(velocity_z, scale=1000, variable='V_z', output=output_path)
+plot_mean_vel_time(velocity_z, time, scale=1000,
+                   variable='V_z', output=output_path)
+
+# %%
+# acceleration_magnitude = [track.amag for track in samples]
+# plot_pdf_log(acceleration_magnitude)
+
+# %%
---
+++ Processing Steps/05_4frame_tracking.py
@@ -0,0 +1,32 @@
+# %%
+from python_4be_eti.perform_tracking import FourFrameTracking
+from python_4be_eti.utils.basic_utils import create_h5_file
+
+# directory containing the file with particle information (location, area, frame number)
+# you must include the full path to the directory here
+folder = '/workspaces/4d-ptv-mcflow/data/julian/PTV_center/TTI_no_gravity/Run2'
+filename = 'rays_out_cpp.h5'
+run = 'run1'
+
+create_h5_file(folder)
+
+# 2d or 3d tracking?
+dimension = '3d'
+# box size in x direction for track initialization (a good initial guess is the expected
+# maximum displacement of the particles in the x direction between frames)
+box_size_initial_x = 2
+# box size in y direction for track initialization
+box_size_initial_y = 2
+# box size in z direction for track initialization
+box_size_initial_z = 2
+# box size used after a track is initialized (this should be as small as possible to
+# eliminate spurious track)
+box_size = 1
+# %%
+tracking = FourFrameTracking(folder, filename=filename, box_size_x=box_size_initial_x,
+                             box_size_y=box_size_initial_y, box_size_z=box_size_initial_z, box_size_track=box_size)
+
+# %%
+tracking.run_tracking(20)
+
+# %%
